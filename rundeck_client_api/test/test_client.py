655cc6a8a2b6b05ca8aa63e1d84f7c89dd290905
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2015-05-20 15:06:09 +0300

    complete first functional test
    This code tries to:
    
    1. import a new job, whose specification is read from a file in
       rundeck. Verify that the job is imported.
    
    2. import a job, whose specification is erroneous, and verify that the
       operation fails
    
    3. run the previously imported job and verify that the execution
       succeeds.
    
    4. delete the job and verify that it no longer exists.

--- rundeck_client_api/test/test_client.py
+++ rundeck_client_api/test/test_client.py
@@ -62,45 +62,67 @@ class TestRundeckClientAPIFunctional:
         # Try to import a good job
         with open('test_data/good_job_definition.xml') as fl:
             good_job = fl.read()
-        status_code, data = self.client.perform_request('import_job', data={'xmlBatch': good_job})
+        status_code, data = self.client.import_job(data={'xmlBatch': good_job})
 
         # Verify that the call returned 200 [OK] and that it successfully created a job
         nt.assert_equal(status_code, 200, 'API call did not return status OK:\n{}'.format(data.find(".//message").text))
-        nt.assert_dict_contains_subset({'count': '1'}, data.find(".//succeeded"),
+        nt.assert_dict_contains_subset({'count': '1'}, data.find(".//succeeded").attrib,
                                        'Failed to create job. Error message:\n{}'.format(data.find('.//error').text))
 
         # Try to import a bad job
         with open('test_data/bad_job_definition.xml') as fl:
             bad_job = fl.read()
-        status_code, data = self.client.perform_request('import_job', data={'xmlBatch': bad_job})
+        status_code, data = self.client.import_job(data={'xmlBatch': bad_job})
 
         # Verify that the call returned 200 [OK] and that it failed to create a job
         nt.assert_equal(status_code, 200, 'API call did not return status OK:\n{}'.format(data.find(".//message").text))
-        nt.assert_dict_contains_subset({'count': '0'}, data.find(".//succeeded"),
+        nt.assert_dict_contains_subset({'count': '0'}, data.find(".//succeeded").attrib,
                                        'Did not fail to create job. API returned:\n{}'.format(self.pretty_print_xml(
                                            data)))
 
         # --Find out what jobs are available
-
-        # Verify that the list jobs endpoint is implemented
-        nt.assert_is_not_none(endpoints.get('list_jobs'))
-        status_code, data = self.client.perform_request('list_jobs', project='')
+        status_code, data = self.client.list_jobs(project='')  # TODO configure project id
 
         # Verify that the call returned status 200
         nt.assert_equal(status_code, 200, 'API call did not return status OK:\n{}'.format(data.find(".//message").text))
 
         # Verify that the newly created job exists in the list
-        nt.assert_in('test_job_2', [j.name for j in data.iterfind('.//name')],
+        nt.assert_in('test_job_2', [j.text for j in data.iterfind('.//name')],
                      'Created job not found. API response:\n{}'.format(self.pretty_print_xml(data)))
 
-        # Run the new job
+        # --Run the new job
+        for job in data.iter("jobs"):
+            if job.find('.//name').text == 'test_job_2':
+                job_id = job.get('id')
 
+        status_code, data = self.client.run_job(id=job_id)
+
+        # Verify that the call returned 200 [OK]
+        nt.assert_equal(status_code, 200, 'API call did not return status OK:\n{}'.format(data.find(".//message").text))
         # Verify the results of the run
+        nt.assert_dict_contains_subset({'count': '1'}, data.attrib,
+                                       'Failed to run job. API returned:\n{}'.format(self.pretty_print_xml(data)))
+        execution_id = data.find(".//execution").get('id')
 
-        # Delete the new job
+        # Verify that the execution succeeded
+        status_code, data = self.client.execution(id=execution_id)
+        nt.assert_dict_contains_subset({'status': 'succeeded'}, data.find('.//execution').attrib)
 
-        # Get the available jobs
+        # Try to run a nonexistent job
+        status_code, data = self.client.run_job(id='123-abc')
+
+        nt.assert_equal(status_code, 404,
+                        'Non existent job run, did not return 404:\n{}'.format(data.find(".//message")))
+        nt.assert_dict_contains_subset({'error': 'true'}, data.attrib,
+                                       'Non existent job run, result is not "error". API returned:{}\n'.format(
+                                           self.pretty_print_xml(data)))
+
+        # --Delete the new job
+        status_code, data = self.client.delete_job(id=job_id)
+
+        nt.assert_equal(status_code, 204,
+                        'Delete request failed. API returned:\n{}'.format(self.pretty_print_xml(data)))
 
         # Verify that the new job has been deleted
-
-
+        status_code, data = self.client.list_jobs(project='')  # TODO configure project id
+        nt.assert_not_in(job_id, [j.get('id') for j in data.iter('job')], 'Job deletion failed.')
