8bbff1c4d08ee8bec4a06e46e1d4602563b33753
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2015-09-11 12:04:35 +0300

    Bump version to 0.3.1

--- pyrundeck/__init__.py
+++ pyrundeck/__init__.py
@@ -31,7 +31,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 __author__ = "Panagiotis Koutsourakis <kutsurak@ekt.gr>"
-__version__ = '0.3'
+__version__ = '0.3.1'
 
 from .api import RundeckApiClient
 from .exceptions import RundeckException
